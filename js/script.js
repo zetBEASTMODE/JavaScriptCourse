32f7c7c038222e321ef040e6845bb8fb6b9009ce
zet BEASTMODE <zet9494@gmail.com>
2022-08-17 21:34:43 +1000

    15 lesson practice part 2

--- js/script.js
+++ js/script.js
@@ -14,12 +14,33 @@ let personalMovieDB = {
     privat: false
 };
 
-let a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+
+
+if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else {
+    alert("Произошла ошибка");
+}
+
+
 
 console.log(personalMovieDB);
